298d1607cb407cf1c80c0dde9d7bdc3a6800051b
s757129 <92375768+s757129@users.noreply.github.com>
2022-01-24 21:56:35 +0800

    Update workflows.py

--- huami/workflows.py
+++ huami/workflows.py
@@ -134,7 +134,7 @@ def main_handler(event, context):
     return main()
 
 if __name__ == '__main__':
-    user = "1*********6" #手机
+    user = "1*********9" #手机
     password = "1******8" #密码
     step = str(randint(17760,19999)) #随机步数
     main()
